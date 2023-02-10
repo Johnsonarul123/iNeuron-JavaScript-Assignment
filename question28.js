285097505fe2f7bf20efa554b4d3d98474867a79
Arul johnson <aruljohnson9411@gmail.com>
2023-02-10 11:22:39 +0530

    completed question28

--- question28.js
+++ question28.js
@@ -13,3 +13,18 @@ for(let i=0; i< 3; i++)
       t += '\n';
 }
 console.log(t);
+
+// b. Print a square pattern, if the input is 3 then the output should be similar to the given output
+// 	   ***
+// 	   ***
+// 	   ***
+
+let a ="";
+
+for(let i=0; i< 3 ; i++){
+    for(let j=0 ; j<3 ;j++){
+        a += "*";
+    }
+    a += '\n';
+}
+console.log(a);
